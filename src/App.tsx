eee0d74334a2d5178ffc5507d809e821eb0e7788
kuancheng_cai <a6232241pccu@gmail.com>
2026-03-17 17:41:10 +0800

    chore(app): 移除未使用的 ErrorBoundary 與 Suspense 相關邏輯

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,8 @@
-import { Suspense, useState, type ErrorInfo } from "react";
-import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 import "./App.css";
 import { TestProvider } from "./contexts/Test";
-import { Posts, About } from "./components";
 import { Link } from "react-router-dom";
 
-const FallbackComponent = ({
-  error,
-  resetErrorBoundary,
-  onReset,
-}: FallbackProps & { onReset: () => void }) => {
-  const handleReset = () => {
-    onReset();
-    resetErrorBoundary();
-  };
-
-  return (
-    <div>
-      <h1>Something went wrong</h1>
-      <p>{error.message}</p>
-      <button onClick={handleReset}>Try again</button>
-    </div>
-  );
-};
-
 function App() {
-  const [tab, setTab] = useState("about");
-  const [isTestError, setIsTestError] = useState(true);
-
-  const postsPromise = fetch("https://jsonplaceholder.typicode.com/posts").then(
-    (res) => {
-      if (!res.ok || isTestError) {
-        throw new Error("Failed to fetch mock data");
-      }
-      return res.json();
-    },
-  );
-  // .catch((error) => {
-  //   if (error instanceof Error) {
-  //     return error.message;
-  //   }
-  //   return "Failed to fetch mock data";
-  // })
-
-  const handleError = (error: unknown, info: ErrorInfo) => {
-    console.log(error);
-    console.log(info);
-  };
-
   return (
     <TestProvider foo="bar" bar={42}>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
@@ -55,20 +10,6 @@ function App() {
         <Link to="/suspense">Suspense</Link>
         <Link to="/suspense-navigation">SuspenseNavigation</Link>
       </div>
-
-      {/* <button onClick={() => setTab("about")}>About</button>
-      <button onClick={() => setTab("posts")}>Posts</button>
-      <ErrorBoundary
-        onError={handleError}
-        fallbackRender={(props) => (
-          <FallbackComponent {...props} onReset={() => setIsTestError(false)} />
-        )}
-      >
-        <Suspense fallback={<div>Loading...</div>}>
-          {tab === "about" && <About isUseContext={false} />}
-          {tab === "posts" && <Posts postsPromise={postsPromise} />}
-        </Suspense>
-      </ErrorBoundary> */}
     </TestProvider>
   );
 }
