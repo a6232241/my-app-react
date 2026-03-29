23b0461fe060b3e2f7258526be3519a23d9f927e
kuancheng_cai <a6232241pccu@gmail.com>
2026-03-29 13:44:43 +0800

    docs(demo): 記錄 Infinity Loading 實作的已知問題與優化方向

--- src/features/InfinityLoading/List.tsx
+++ src/features/InfinityLoading/List.tsx
@@ -2,6 +2,13 @@ import { useState, useRef, useCallback, Suspense } from "react";
 import { fetchData } from "../../utils/data";
 import { Post } from "../../components/ui";
 
+// 模擬無限滾動
+// 已知問題：
+// 1. 由於 promise 是各自管理，如果頁數已經到底，會無法知道，導致一直發送請求，需要一個可以判斷頁數的機制，來避免該問題
+// 2. 沒有提前獲取資料，會導致滑動不夠絲滑，可以修改 handleScroll 提前獲取資料
+// 3. 當前結構是遍歷每頁(promise)，而每頁在遍歷資料(Post)，不夠扁平，可能有效能問題
+// 4. 預設的 Loading 沒有預估高度，會導致視覺跳躍，可以預先設定高度解決該問題
+
 const List = () => {
   const [items, setItems] = useState<
     { id: string; promise: Promise<unknown> }[]
